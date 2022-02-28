0087694696395c713f15d63a98055843d2336062
Almas Khan <almas.rbm@gmail.com>
2022-03-01 04:04:36 +0600

    diplaying phone done

--- js/app.js
+++ js/app.js
@@ -2,26 +2,32 @@ document.getElementById('btn-phones').addEventListener('click', () => {
     //getting search value...
     const searchInput = document.getElementById('phone-input');
     const searchText = searchInput.value;
-    //fetching url
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhones(data.data))
+    if (searchText !== '') {
+        searchInput.value = '';
+        //fetching url
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhones(data.data))
+    }
+    else {
+        errorMessage('error-msg', 'block');
+    }
 });
 //displaying phone
 const displayPhones = phones => {
     const searchResult = document.getElementById('display-phones');
     //clearing previous display result
-    //searchResult.textContent = '';
+    searchResult.textContent = '';
     if (phones.length !== 0) {
         phones.forEach(phone => {
             //console.log(phone);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-                <div class="card h-100">
-                    <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
-                    <div class="card-body">
+                <div class="card h-100 border-dark">
+                    <img src="${phone.image}" class="card-img-top w-100 img-fluid" alt="...">
+                    <div class="card-body text-success">
                         <h3 class="card-text">${phone.phone_name}</h3>
                         <h3 class="card-text">${phone.brand}</h3>
                         <button onclick="details('${phone.slug}')" type="button" class="details-btn w-100 mx-auto fs-5">Details</button>
